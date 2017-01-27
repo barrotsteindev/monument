683e90f265449f5f4a9aab1f3270f515a7b3228d
Bar Rotstein <barrotstein@gmail.com>
2017-01-27 16:19:05 +0200

    bad multipart/form-data test is working

--- utils/parser_test.js
+++ utils/parser_test.js
@@ -48,24 +48,20 @@ describe('Parser Tests', () => {
         });
     });
 
-
     it('should return an error for wrong multipart/form-data submission', (done) => {
-        const form = new FormData();
+        const fakeForm = {
+                           headers:
+                             {
+                               'content-type': 'multipart/form-data;'
+                             }
+                         };
 
-        form.append('cont', 'some random content');
-        //  form.append('title', 'lord of the interwebz');
-
-        form.headers = form.getHeaders();
-
-        parser({ req: form }, (body, err) => {
+        parser({ req: fakeForm }, (body, err) => {
             assert.instanceOf(err, Error);
             assert.isNull(body);
             done();
         });
     });
-
-
-
 
     it('should parse out a application/x-www-form-urlencoded submission', (done) => {
         stream.push(urlBody);
